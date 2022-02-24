b136df897bdb044511fbda3aa937b3a021161707
mentoriadotnet <99259265+mentoriadotnet@users.noreply.github.com>
2022-02-24 14:04:45 -0300

    Add files via upload

---
+++ InjDependecia.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjecaoDependencia
+{
+    public interface IAnimal { void Comer(); }
+
+    public class Cachorros : IAnimal
+    {
+        public void Comer()
+        {
+            //
+        }
+    }
+
+    public class Gatos : IAnimal
+    {
+        public void Comer()
+        {
+            //
+        }
+    }
+
+    public class PetShop
+    {
+        private IAnimal Pet;
+
+        public IAnimal PetAtual;
+
+        public PetShop()
+        {
+            //
+        }
+
+        public PetShop(IAnimal Animal)
+        {
+            this.Pet = Animal;
+        }
+
+        public void DarDeComer()
+        {
+            Pet.Comer();
+        }
+
+        public void DarBanho()
+        {
+            // Usar PetAtual
+        }
+
+        public void AplicarVacina(IAnimal AnimalNaFila)
+        {
+            // User AnimalNaFila
+        }
+
+    }
+
+    public class Dono
+    {
+        Cachorros MeuDog = new Cachorros();
+        
+        public void CuidarDoMeuBichinho()
+        {
+            PetShop Alimentar = new PetShop(MeuDog);
+            Alimentar.DarDeComer();
+
+            PetShop Banhar = new PetShop();
+            Banhar.PetAtual = MeuDog;
+            Banhar.DarBanho();
+
+            PetShop Vacinar = new PetShop();
+            Vacinar.AplicarVacina(MeuDog);
+        }
+
+    }
+}
+
+
+
+
+
+
+
